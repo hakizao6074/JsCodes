7c7607ad382cb0330b0e72c4d11a875f26f8cab9
hakizao6074 <davizinhodjar@gmail.com>
2025-11-10 07:28:05 -0300

    push indexof

---
+++ arrays/examples/code-examples/LastInedxOf/lastIndexOf.js
@@ -0,0 +1,6 @@
+// Criando um array com números, note que o número 20 aparece duas vezes
+let numeros = [10,20,30,20,40]
+
+// lastIndexOf(20) retorna o índice da ÚLTIMA ocorrência do elemento 20 no array
+// Neste caso, retornará 3, pois é a posição da última ocorrência do número 20
+console.log(numeros.lastIndexOf(20)) // Retornará 3
---
+++ arrays/examples/code-examples/indexOf/1-indexOff.js
@@ -0,0 +1,9 @@
+// Criando um array com números
+let numeros = [10,20,30,40,50]
+
+// indexOf(20) retorna o índice do elemento 20 no array (que é 1)
+console.log(numeros.indexOf(20))
+
+
+// indexOf(10) retorna o índice do elemento 10 no array (que é 0)
+console.log(numeros.indexOf(10)) // Retornará 0
