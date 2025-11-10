79f3d4f6ace8f5544f4751a3cb06718c78ad0c8a
hakizao6074 <davizinhodjar@gmail.com>
2025-11-10 11:10:51 -0300

    push

---
+++ arrays/examples/code-examples/fillter/4-filter.js
@@ -0,0 +1,5 @@
+let palavras = ["maça", "mexirica", "banana", "uva"]
+let palavrasComMu = palavras.filter(palavras => palavras.startsWith("m") + palavras.startsWith("u"));
+
+console.log(palavras)
+console.log(palavrasComMu)
