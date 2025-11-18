7aa0fd493412c53bdca40f13845e8c5494a73d71
hakizao6074 <davizinhodjar@gmail.com>
2025-11-18 11:30:20 -0300

    push

--- list-senai/functionAssicronas/exemplo-8.js
+++ list-senai/functionAssicronas/exemplo-8.js
@@ -1,3 +1,24 @@
-async function nome() {
-  return new Promise
+async function alunoarrumando(nome, tempo) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+        resolve(`${nome} esta pronto`)
+    }, tempo)
+  });
 }
+
+async function passeioEscolar() {
+    console.log("O onibus esta esperando os alunos ficarem prontos...")
+
+    const [aluno1, aluno2, aluno3] = await Promise.all([
+        alunoarrumando("João", 2000),
+        alunoarrumando("Maria", 3000),
+        alunoarrumando("Lucas", 1500)
+    ]);
+    console.log(aluno1)
+    console.log(aluno2)
+    console.log(aluno3)
+
+    console.log("Todos prontos! O onibus pode partir")
+}
+
+passeioEscolar()
